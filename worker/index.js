f4ed35f1a75bd6fc65eb8ff2d416ee3ca847a214
SpaGettys <jgettys@gmail.com>
2019-05-29 19:19:34 +0900

    Initial Commit

---
+++ worker/index.js
@@ -0,0 +1,21 @@
+const keys = require('./keys');
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+});
+const sub = redisClient.duplicate();
+
+// We chose a slow recursive slution to calculate fib so we can simulate separate worker process (slow process) better.
+function fib(index) {
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
+}
+
+sub.on('message', (channel, message) => {
+  redisClient.hset('values', message, fib(parseInt(message)));
+});
+// subscribe to redis insert events
+sub.subscribe('insert');
